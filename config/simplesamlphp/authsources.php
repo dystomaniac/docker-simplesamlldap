f0afdcb3fedc3645823abaf8f98c56bb26beb4d7
skapxdev <sikomde3000@gmail.com>
2023-11-17 12:13:13 +0100

    workflow to publish package

--- config/simplesamlphp/authsources.php
+++ config/simplesamlphp/authsources.php
@@ -8,10 +8,9 @@ $config = array(
     'ldap' => [
         'ldap:LDAP',
         'hostname' => 'ldap.movieking.com',
-        'enable_tls' => true,
+        'enable_tls' => false,
         'timeout' => 10,
         'dnpattern' => 'cn=%username%,dc=movieking,dc=com',
         'search.enable' => false,
     ],
-
 );
